6238cb02f619ee09efe5a44f78eb9390847fee61
kenji <kgfahselt@gmail.com>
2025-07-03 10:01:01 -0700

    edited launch sim for ignition

--- launch/launch_sim.py
+++ launch/launch_sim.py
@@ -14,34 +14,44 @@ from launch_ros.actions import Node
 def generate_launch_description():
 
 
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
+    package_name = 'my_bot'  # Your package name
 
-    package_name='ros2launch' #<--- CHANGE ME
-
+    # Launch robot_state_publisher (URDF -> /robot_description)
     rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory(package_name),
+                'launch',
+                'rsp.launch.py'
+            )
+        ),
+        launch_arguments={'use_sim_time': 'true'}.items()
     )
 
-    # Include the Gazebo launch file, provided by the gazebo_ros package
-    gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-             )
+    # Launch Gazebo Harmonic (Ignition Gazebo Sim)
+    gz_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('ros_gz_sim'),
+                'launch',
+                'gz_sim.launch.py'
+            )
+        )
+    )
 
-    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-topic', 'robot_description',
-                                   '-entity', 'my_bot'],
-                        output='screen')
+    # Spawn the robot into Gazebo using /robot_description
+    spawn_entity = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=[
+            '-name', 'my_bot',
+            '-topic', 'robot_description'
+        ],
+        output='screen'
+    )
 
-
-
-    # Launch them all!
     return LaunchDescription([
         rsp,
-        gazebo,
-        spawn_entity,
+        gz_sim,
+        spawn_entity
     ])
